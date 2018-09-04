55b6265993c3ff71ebebf9a5a96cc3c649b57ada
lr <laggingreflex@gmail.com>
2018-09-04 12:44:02 +0530

    [WIP] Refactor

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const gfork = require('..');
+const yargs = require('yargs');
+
+const argv = yargs.options()
--- jest.config.js
+++
@@ -1,4 +0,0 @@
-
-exports.transform = {};
-
-exports.collectCoverage = true;
